3af9760d4b3ab414e27b91eb3eeb5947f97d4961
kmsu <1693936230101@ingenieria.usac.edu.gt>
2025-08-07 17:22:56 -0600

    [ADD] Agregar producto
    Se corrigió un error en el menú

--- P1/main.py
+++ P1/main.py
@@ -1,42 +1,84 @@
+#Clase para crear productos
+class Producto:
+    def __init__(self, nombre, cantidad, precio):
+        self.nombre = nombre
+        self.cantidad = cantidad
+        self.precio = precio
+
+    #__str__ Es un metodo de python como mostrar el objeto para imprimirlo o verlo en consola
+    def __str__(self):
+        return f"{self.nombre} - {self.cantidad} unidades - Q{self.precio:.2f}"
 
 #Para mostrar el menu, es la funcion predeterminada del programa
 def main():
     # Lista que guarda los productos (inventario) 
-    inventario = []
+    listaProductos = []
 
     while True:
         mostrar_menu()
         opcion = input("Seleccione una opción (1-6): ")
 
         if opcion == "1":
-            agregar_producto(inventario)
+            agregar_producto(listaProductos)
         elif opcion == "2":
-            eliminar_producto(inventario)
+            eliminar_producto(listaProductos)
         elif opcion == "3":
-            mostrar_productos(inventario)
+            mostrar_productos(listaProductos)
         elif opcion == "4":
-            ordenar_productos(inventario)
+            ordenar_productos(listaProductos)
         elif opcion == "5":
-            buscar_producto(inventario)
+            buscar_producto(listaProductos)
         elif opcion == "6":
-            print("👋 Saliendo del programa. ¡Hasta luego!")
+            print("¡Hasta luego!")
             break
         else:
-            print("❌ Opción inválida. Intente de nuevo.")
+            print("Opción inválida. Intente de nuevo.")
 
-def agregar_producto(inventario):
-    print("agregar producto")
+def agregar_producto(listaProductos):
+    # Ingresa un nuevo producto
+    nombre = input("Ingrese el nombre del producto: ").strip()
 
-def eliminar_producto(inventario):
+    #Validar que el nombre no se ingrese vacio
+    if not nombre:
+        print("El nombre no puede estar en blanco.")
+        return
+    
+    #Validar que el producto no exista en el inventario
+    for producto in listaProductos:
+        if producto.nombre.lower() == nombre.lower():
+            print("El producto ya existe")
+            return
+    
+    # Ingresar la cantidad y el precio y convertirlo en un valor numerico
+    try:
+        cantidad = int(input("Ingrese la cantidad: "))
+        precio = float(input("Ingrese el precio: "))
+    except ValueError:
+        print("Entrada invalida. Cantidad debe ser entero y precio debe ser decimal.")
+    
+    #Crear el producto
+    nuevoProducto = Producto(nombre, cantidad, precio)
+    #Agregar a la lista de productos (inventario)
+    listaProductos.append(nuevoProducto)
+    print("Producto agregado correctamente")
+
+def eliminar_producto(listaProductos):
     print("eliminar producto")
 
-def mostrar_productos(inventario):
-    print("mostrar producto")
+def mostrar_productos(listaProductos):
+    #Validar si la lista (inventario) esta vacio
+    if not listaProductos:
+        print("No hay productos en el inventario")
+        return
+    
+    print("\n LISTA DE PRODUCTOS")
+    for id, producto in enumerate(listaProductos, start=1):
+        print(f"{id}. Nombre: {producto.nombre} | Cantidad: {producto.cantidad} | Precio: Q{producto.precio:.2f}")
 
-def ordenar_productos(inventario):
+def ordenar_productos(listaProductos):
     print("ordenar producto")
 
-def buscar_producto(inventario):
+def buscar_producto(listaProductos):
     print("buscar producto")
 
 def mostrar_menu():
